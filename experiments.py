e1794fa8d50614c1ec7e6fc4c09c3adebb8f3e27
Gregory Ditzler <gregory.ditzler@gmail.com>
2021-04-08 11:50:19 -0400

    now collecting the results. we need to save them and find a more efficient way to store them

--- experiments.py
+++ experiments.py
@@ -66,6 +66,34 @@ def run_experiment_exploratory(dataset:str='unswnb15',
     # we need to set up the k-fold evaluator 
     kf = KFold(n_splits=trials)
 
+    accs_if_baseline, fss_if_baseline, tprs_if_baseline, tnrs_if_baseline, mccs_if_baseline = 0., 0., 0., 0., 0.
+    accs_svm_baseline, fss_svm_baseline, tprs_svm_baseline, tnrs_svm_baseline, mccs_svm_baseline = 0., 0., 0., 0., 0. 
+    accs_ee_baseline, fss_ee_baseline, tprs_ee_baseline, tnrs_ee_baseline, mccs_ee_baseline = 0., 0., 0., 0., 0.
+    accs_lo_baseline, fss_lo_baseline, tprs_lo_baseline, tnrs_lo_baseline, mccs_lo_baseline = 0., 0., 0., 0., 0.
+
+    accs_if_deepfool, fss_if_deepfool, tprs_if_deepfool, tnrs_if_deepfool, mccs_if_deepfool = 0., 0., 0., 0., 0.
+    accs_svm_deepfool, fss_svm_deepfool, tprs_svm_deepfool, tnrs_svm_deepfool, mccs_svm_deepfool = 0., 0., 0., 0., 0. 
+    accs_ee_deepfool, fss_ee_deepfool, tprs_ee_deepfool, tnrs_ee_deepfool, mccs_ee_deepfool = 0., 0., 0., 0., 0.
+    accs_lo_deepfool, fss_lo_deepfool, tprs_lo_deepfool, tnrs_lo_deepfool, mccs_lo_deepfool = 0., 0., 0., 0., 0.
+
+    accs_if_fgsm, fss_if_fgsm, tprs_if_fgsm, tnrs_if_fgsm, mccs_if_fgsm = 0., 0., 0., 0., 0.
+    accs_svm_fgsm, fss_svm_fgsm, tprs_svm_fgsm, tnrs_svm_fgsm, mccs_svm_fgsm = 0., 0., 0., 0., 0. 
+    accs_ee_fgsm, fss_ee_fgsm, tprs_ee_fgsm, tnrs_ee_fgsm, mccs_ee_fgsm = 0., 0., 0., 0., 0.
+    accs_lo_fgsm, fss_lo_fgsm, tprs_lo_fgsm, tnrs_lo_fgsm, mccs_lo_fgsm = 0., 0., 0., 0., 0.
+
+    accs_if_pgd, fss_if_pgd, tprs_if_pgd, tnrs_if_pgd, mccs_if_pgd = 0., 0., 0., 0., 0.
+    accs_svm_pgd, fss_svm_pgd, tprs_svm_pgd, tnrs_svm_pgd, mccs_svm_pgd = 0., 0., 0., 0., 0. 
+    accs_ee_pgd, fss_ee_pgd, tprs_ee_pgd, tnrs_ee_pgd, mccs_ee_pgd = 0., 0., 0., 0., 0.
+    accs_lo_pgd, fss_lo_pgd, tprs_lo_pgd, tnrs_lo_pgd, mccs_lo_pgd = 0., 0., 0., 0., 0.
+
+    accs_if_dt, fss_if_dt, tprs_if_dt, tnrs_if_dt, mccs_if_dt = 0., 0., 0., 0., 0.
+    accs_svm_dt, fss_svm_dt, tprs_svm_dt, tnrs_svm_dt, mccs_svm_dt = 0., 0., 0., 0., 0. 
+    accs_ee_dt, fss_ee_dt, tprs_ee_dt, tnrs_ee_dt, mccs_ee_dt = 0., 0., 0., 0., 0.
+    accs_lo_dt, fss_lo_dt, tprs_lo_dt, tnrs_lo_dt, mccs_lo_dt = 0., 0., 0., 0., 0.
+
+
+
+
     for train_index, _ in kf.split(X_tr):
         # split the original data into training / testing datasets. we are not going to 
         # use the testing data since we are not going to learn a classifier. 
@@ -76,68 +104,267 @@ def run_experiment_exploratory(dataset:str='unswnb15',
 
         # isolation forest 
         model = IsolationForest(contamination=contamination).fit(X_tr_n_normal)
-        y_if = model.predict(X_te)
-        y_if_deepfool = model.predict(X_adv_deepfool)
-        y_if_fgsm = model.predict(X_adv_fgsm)
-        y_if_pgd = model.predict(X_adv_pgd)
-        y_if_dt = model.predict(X_adv_dt)
-
+        y_if, y_if_deepfool, y_if_fgsm, y_if_pgd, y_if_dt = model.predict(X_te), model.predict(X_adv_deepfool), \
+            model.predict(X_adv_fgsm), model.predict(X_adv_pgd), model.predict(X_adv_dt)
+        
         # osvm 
         model = OneClassSVM(kernel='poly', degree=degree).fit(X_tr_n_normal)
-        y_svm = model.predict(X_te)
-        y_svm_deepfool = model.predict(X_adv_deepfool)
-        y_svm_fgsm = model.predict(X_adv_fgsm)
-        y_svm_pgd = model.predict(X_adv_pgd)
-        y_svm_dt = model.predict(X_adv_dt)
+        y_svm, y_svm_deepfool, y_svm_fgsm, y_svm_pgd, y_svm_dt = model.predict(X_te), model.predict(X_adv_deepfool), \
+            model.predict(X_adv_fgsm), model.predict(X_adv_pgd), model.predict(X_adv_dt)
 
         # elliiptic 
         model = EllipticEnvelope(contamination=contamination, support_fraction=support_fraction).fit(X_tr_n_normal)
-        y_ee = model.predict(X_te)
-        y_ee_deepfool = model.predict(X_adv_deepfool)
-        y_ee_fgsm = model.predict(X_adv_fgsm)
-        y_ee_pgd = model.predict(X_adv_pgd)
-        y_ee_dt = model.predict(X_adv_dt)
+        y_ee, y_ee_deepfool, y_ee_fgsm, y_ee_pgd, y_ee_dt = model.predict(X_te), model.predict(X_adv_deepfool), \
+            model.predict(X_adv_fgsm), model.predict(X_adv_pgd), model.predict(X_adv_dt)
 
         # local outliers
-        model = LocalOutlierFactor(contamination=0.05).fit(X_tr_n_normal)
+        model = LocalOutlierFactor(contamination=contamination).fit(X_tr_n_normal)
+        # the novelty flag needs to be set to run
         model.novelty = True
+        y_lo, y_lo_deepfool, y_lo_fgsm, y_lo_pgd, y_lo_dt = model.predict(X_te), model.predict(X_adv_deepfool), \
+            model.predict(X_adv_fgsm), model.predict(X_adv_pgd), model.predict(X_adv_dt)
 
-        y_lo = model.predict(X_te)
-        y_lo_deepfool = model.predict(X_adv_deepfool)
-        y_lo_fgsm = model.predict(X_adv_fgsm)
-        y_lo_pgd = model.predict(X_adv_pgd)
-        y_lo_dt = model.predict(X_adv_dt)
 
         acc_if_baseline, fs_if_baseline, tpr_if_baseline, tnr_if_baseline, mcc_if_baseline = get_performance(y_true=y_te, y_hat=y_if)
         acc_svm_baseline, fs_svm_baseline, tpr_svm_baseline, tnr_svm_baseline, mcc_svm_baseline = get_performance(y_true=y_te, y_hat=y_svm)
         acc_ee_baseline, fs_ee_baseline, tpr_ee_baseline, tnr_ee_baseline, mcc_ee_baseline = get_performance(y_true=y_te, y_hat=y_ee)
-        acc_lo_baseline, fs_loo_baseline, tpr_lo_baseline, tnr_lo_baseline, mcc_lo_baseline = get_performance(y_true=y_te, y_hat=y_lo)
-
+        acc_lo_baseline, fs_lo_baseline, tpr_lo_baseline, tnr_lo_baseline, mcc_lo_baseline = get_performance(y_true=y_te, y_hat=y_lo)
+        
+        accs_if_baseline += acc_if_baseline 
+        fss_if_baseline += fs_if_baseline 
+        tprs_if_baseline += tpr_if_baseline
+        tnrs_if_baseline += tnr_if_baseline
+        mccs_if_baseline +=mcc_if_baseline 
+        accs_svm_baseline += acc_svm_baseline
+        fss_svm_baseline  +=fs_svm_baseline 
+        tprs_svm_baseline += tpr_svm_baseline 
+        tnrs_svm_baseline += tnr_svm_baseline
+        mccs_svm_baseline +=mcc_svm_baseline
+        accs_ee_baseline  +=acc_ee_baseline 
+        fss_ee_baseline  +=fs_ee_baseline 
+        tprs_ee_baseline += tpr_ee_baseline
+        tnrs_ee_baseline += tnr_ee_baseline 
+        mccs_ee_baseline +=mcc_ee_baseline
+        accs_lo_baseline += acc_lo_baseline 
+        fss_lo_baseline += fs_lo_baseline 
+        tprs_lo_baseline += tpr_lo_baseline 
+        tnrs_lo_baseline += tnr_lo_baseline
+        mccs_lo_baseline += mcc_lo_baseline
         
         acc_if_deepfool, fs_if_deepfool, tpr_if_deepfool, tnr_if_deepfool, mcc_if_deepfool = get_performance(y_true=y_te, y_hat=y_if_deepfool)
         acc_svm_deepfool, fs_svm_deepfool, tpr_svm_deepfool, tnr_svm_deepfool, mcc_svm_deepfool = get_performance(y_true=y_te, y_hat=y_svm_deepfool)
         acc_ee_deepfool, fs_ee_deepfool, tpr_ee_deepfool, tnr_ee_deepfool, mcc_ee_deepfool = get_performance(y_true=y_te, y_hat=y_ee_deepfool)
         acc_lo_deepfool, fs_lo_deepfool, tpr_lo_deepfool, tnr_lo_deepfool, mcc_lo_deepfool = get_performance(y_true=y_te, y_hat=y_lo_deepfool)
 
+        accs_if_deepfool += acc_if_deepfool 
+        fss_if_deepfool += fs_if_deepfool 
+        tprs_if_deepfool += tpr_if_deepfool
+        tnrs_if_deepfool += tnr_if_deepfool
+        mccs_if_deepfool +=mcc_if_deepfool 
+        accs_svm_deepfool += acc_svm_deepfool
+        fss_svm_deepfool  +=fs_svm_deepfool 
+        tprs_svm_deepfool += tpr_svm_deepfool 
+        tnrs_svm_deepfool += tnr_svm_deepfool
+        mccs_svm_deepfool +=mcc_svm_deepfool
+        accs_ee_deepfool  +=acc_ee_deepfool 
+        fss_ee_deepfool  +=fs_ee_deepfool 
+        tprs_ee_deepfool += tpr_ee_deepfool
+        tnrs_ee_deepfool += tnr_ee_deepfool 
+        mccs_ee_deepfool +=mcc_ee_deepfool
+        accs_lo_deepfool += acc_lo_deepfool 
+        fss_lo_deepfool += fs_lo_deepfool 
+        tprs_lo_deepfool += tpr_lo_deepfool 
+        tnrs_lo_deepfool += tnr_lo_deepfool
+        mccs_lo_deepfool += mcc_lo_deepfool
+
+
         acc_if_fgsm, fs_if_fgsm, tpr_if_fgsm, tnr_if_fgsm, mcc_if_fgsm = get_performance(y_true=y_te, y_hat=y_if_fgsm)
         acc_svm_fgsm, fs_svm_fgsm, tpr_svm_fgsm, tnr_svm_fgsm, mcc_svm_fgsm = get_performance(y_true=y_te, y_hat=y_svm_fgsm)
         acc_ee_fgsm, fs_ee_fgsm, tpr_ee_fgsm, tnr_ee_fgsm, mcc_ee_fgsm = get_performance(y_true=y_te, y_hat=y_ee_fgsm)
         acc_lo_fgsm, fs_lo_fgsm, tpr_lo_fgsm, tnr_lo_fgsm, mcc_lo_fgsm = get_performance(y_true=y_te, y_hat=y_lo_fgsm)
+
+        accs_if_fgsm += acc_if_fgsm 
+        fss_if_fgsm += fs_if_fgsm 
+        tprs_if_fgsm += tpr_if_fgsm
+        tnrs_if_fgsm += tnr_if_fgsm
+        mccs_if_fgsm +=mcc_if_fgsm 
+        accs_svm_fgsm += acc_svm_fgsm
+        fss_svm_fgsm  +=fs_svm_fgsm 
+        tprs_svm_fgsm += tpr_svm_fgsm 
+        tnrs_svm_fgsm += tnr_svm_fgsm
+        mccs_svm_fgsm +=mcc_svm_fgsm
+        accs_ee_fgsm  +=acc_ee_fgsm 
+        fss_ee_fgsm  +=fs_ee_fgsm 
+        tprs_ee_fgsm += tpr_ee_fgsm
+        tnrs_ee_fgsm += tnr_ee_fgsm 
+        mccs_ee_fgsm +=mcc_ee_fgsm
+        accs_lo_fgsm += acc_lo_fgsm 
+        fss_lo_fgsm += fs_lo_fgsm 
+        tprs_lo_fgsm += tpr_lo_fgsm 
+        tnrs_lo_fgsm += tnr_lo_fgsm
+        mccs_lo_fgsm += mcc_lo_fgsm
+
 
         acc_if_pgd, fs_if_pgd, tpr_if_pgd, tnr_if_pgd, mcc_if_pgd = get_performance(y_true=y_te, y_hat=y_if_pgd)
         acc_svm_pgd, fs_svm_pgd, tpr_svm_pgd, tnr_svm_pgd, mcc_svm_pgd = get_performance(y_true=y_te, y_hat=y_svm_pgd)
         acc_ee_pgd, fs_ee_pgd, tpr_ee_pgd, tnr_ee_pgd, mcc_ee_pgd = get_performance(y_true=y_te, y_hat=y_ee_pgd)
         acc_lo_pgd, fs_lo_pgd, tpr_lo_pgd, tnr_lo_pgd, mcc_lo_pgd = get_performance(y_true=y_te, y_hat=y_lo_pgd)
 
+        accs_if_pgd += acc_if_pgd 
+        fss_if_pgd += fs_if_pgd 
+        tprs_if_pgd += tpr_if_pgd
+        tnrs_if_pgd += tnr_if_pgd
+        mccs_if_pgd +=mcc_if_pgd 
+        accs_svm_pgd += acc_svm_pgd
+        fss_svm_pgd  +=fs_svm_pgd 
+        tprs_svm_pgd += tpr_svm_pgd 
+        tnrs_svm_pgd += tnr_svm_pgd
+        mccs_svm_pgd +=mcc_svm_pgd
+        accs_ee_pgd  +=acc_ee_pgd 
+        fss_ee_pgd  +=fs_ee_pgd 
+        tprs_ee_pgd += tpr_ee_pgd
+        tnrs_ee_pgd += tnr_ee_pgd 
+        mccs_ee_pgd +=mcc_ee_pgd
+        accs_lo_pgd += acc_lo_pgd 
+        fss_lo_pgd += fs_lo_pgd 
+        tprs_lo_pgd += tpr_lo_pgd 
+        tnrs_lo_pgd += tnr_lo_pgd
+        mccs_lo_pgd += mcc_lo_pgd
+
         acc_if_dt, fs_if_dt, tpr_if_dt, tnr_if_dt, mcc_if_dt = get_performance(y_true=y_te, y_hat=y_if_dt)
         acc_svm_dt, fs_svm_dt, tpr_svm_dt, tnr_svm_dt, mcc_svm_dt = get_performance(y_true=y_te, y_hat=y_svm_dt)
         acc_ee_dt, fs_ee_dt, tpr_ee_dt, tnr_ee_dt, mcc_ee_dt = get_performance(y_true=y_te, y_hat=y_ee_dt)
         acc_lo_dt, fs_lo_dt, tpr_lo_dt, tnr_lo_dt, mcc_lo_dt = get_performance(y_true=y_te, y_hat=y_lo_dt)
 
+        accs_if_dt += acc_if_dt 
+        fss_if_dt += fs_if_dt 
+        tprs_if_dt += tpr_if_dt
+        tnrs_if_dt += tnr_if_dt
+        mccs_if_dt +=mcc_if_dt 
+        accs_svm_dt += acc_svm_dt
+        fss_svm_dt  +=fs_svm_dt 
+        tprs_svm_dt += tpr_svm_dt 
+        tnrs_svm_dt += tnr_svm_dt
+        mccs_svm_dt +=mcc_svm_dt
+        accs_ee_dt  +=acc_ee_dt 
+        fss_ee_dt  +=fs_ee_dt 
+        tprs_ee_dt += tpr_ee_dt
+        tnrs_ee_dt += tnr_ee_dt 
+        mccs_ee_dt +=mcc_ee_dt
+        accs_lo_dt += acc_lo_dt 
+        fss_lo_dt += fs_lo_dt 
+        tprs_lo_dt += tpr_lo_dt 
+        tnrs_lo_dt += tnr_lo_dt
+        mccs_lo_dt += mcc_lo_dt
 
 
 
+    accs_if_baseline /= trials 
+    fss_if_baseline /= trials
+    tprs_if_baseline /= trials
+    tnrs_if_baseline /= trials
+    mccs_if_baseline /= trials
+    accs_svm_baseline /= trials
+    fss_svm_baseline  /= trials
+    tprs_svm_baseline /= trials
+    tnrs_svm_baseline /= trials
+    mccs_svm_baseline /= trials
+    accs_ee_baseline  /= trials
+    fss_ee_baseline  /= trials
+    tprs_ee_baseline /= trials
+    tnrs_ee_baseline /= trials
+    mccs_ee_baseline /= trials
+    accs_lo_baseline /= trials
+    fss_lo_baseline /= trials
+    tprs_lo_baseline /= trials
+    tnrs_lo_baseline /= trials
+    mccs_lo_baseline /= trials
 
-    
+
+    accs_if_deepfool /= trials 
+    fss_if_deepfool /= trials
+    tprs_if_deepfool /= trials
+    tnrs_if_deepfool /= trials
+    mccs_if_deepfool /= trials
+    accs_svm_deepfool /= trials
+    fss_svm_deepfool  /= trials
+    tprs_svm_deepfool /= trials
+    tnrs_svm_deepfool /= trials
+    mccs_svm_deepfool /= trials
+    accs_ee_deepfool  /= trials
+    fss_ee_deepfool  /= trials
+    tprs_ee_deepfool /= trials
+    tnrs_ee_deepfool /= trials
+    mccs_ee_deepfool /= trials
+    accs_lo_deepfool /= trials
+    fss_lo_deepfool /= trials
+    tprs_lo_deepfool /= trials
+    tnrs_lo_deepfool /= trials
+    mccs_lo_deepfool /= trials
+
+
+    accs_if_dt /= trials 
+    fss_if_dt /= trials
+    tprs_if_dt /= trials
+    tnrs_if_dt /= trials
+    mccs_if_dt /= trials
+    accs_svm_dt /= trials
+    fss_svm_dt  /= trials
+    tprs_svm_dt /= trials
+    tnrs_svm_dt /= trials
+    mccs_svm_dt /= trials
+    accs_ee_dt  /= trials
+    fss_ee_dt  /= trials
+    tprs_ee_dt /= trials
+    tnrs_ee_dt /= trials
+    mccs_ee_dt /= trials
+    accs_lo_dt /= trials
+    fss_lo_dt /= trials
+    tprs_lo_dt /= trials
+    tnrs_lo_dt /= trials
+    mccs_lo_dt /= trials
+
+    accs_if_fgsm /= trials 
+    fss_if_fgsm /= trials
+    tprs_if_fgsm /= trials
+    tnrs_if_fgsm /= trials
+    mccs_if_fgsm /= trials
+    accs_svm_fgsm /= trials
+    fss_svm_fgsm  /= trials
+    tprs_svm_fgsm /= trials
+    tnrs_svm_fgsm /= trials
+    mccs_svm_fgsm /= trials
+    accs_ee_fgsm  /= trials
+    fss_ee_fgsm  /= trials
+    tprs_ee_fgsm /= trials
+    tnrs_ee_fgsm /= trials
+    mccs_ee_fgsm /= trials
+    accs_lo_fgsm /= trials
+    fss_lo_fgsm /= trials
+    tprs_lo_fgsm /= trials
+    tnrs_lo_fgsm /= trials
+    mccs_lo_fgsm /= trials
+
+    accs_if_pgd /= trials 
+    fss_if_pgd /= trials
+    tprs_if_pgd /= trials
+    tnrs_if_pgd /= trials
+    mccs_if_pgd /= trials
+    accs_svm_pgd /= trials
+    fss_svm_pgd  /= trials
+    tprs_svm_pgd /= trials
+    tnrs_svm_pgd /= trials
+    mccs_svm_pgd /= trials
+    accs_ee_pgd  /= trials
+    fss_ee_pgd  /= trials
+    tprs_ee_pgd /= trials
+    tnrs_ee_pgd /= trials
+    mccs_ee_pgd /= trials
+    accs_lo_pgd /= trials
+    fss_lo_pgd /= trials
+    tprs_lo_pgd /= trials
+    tnrs_lo_pgd /= trials
+    mccs_lo_pgd /= trials
+
 
     return None 
