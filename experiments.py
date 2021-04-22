13a2ff28a64cf1d3321fe8e203eee7c9dde25580
Gregory Ditzler <gregory.ditzler@gmail.com>
2021-04-22 08:10:11 -0700

    the exp fcn now allows you to choose the experiment you want to run

--- experiments.py
+++ experiments.py
@@ -33,35 +33,47 @@ from utils import get_performance
 
 def run_experiment_exploratory(dataset:str='unswnb15', 
                                trials:int=10, 
+                               type:str='attacks_all', 
                                verbose:bool=False): 
     """Run the experiment for exploratory attacks against intrusion detection. 
+
+    This function runs the multiple attacks against several different detection algorithms
+    which are OneClassSVM, IsolationForest, LocalOutlierFactor and EllipticEnvelope. The 
+    performances are ACC, TPR, TNR and MCC. The function returns nothing, but will write 
+    files to 'outputs/'
+
+    param: dataset 
+    param: trials
+    param: type
+    param: verbose 
     """
 
     if verbose: 
         print(''.join(['Dataset: ', dataset]))
     
+    # detection algorithm specific parameters 
     support_fraction = .5
     contamination = .05
     degree = 3
 
-    OUTPUT_FILE = ''.join(['outputs/results_ids_', dataset, '.npz'])
+    OUTPUT_FILE = ''.join(['outputs/results_ids_', type, '_', dataset, '.npz'])
 
     # load the data from the npz files. note that all of the X_tr, X_te, y_tr and y_te are the same 
     # regarless of the file. the difference is in how the Xaml data are generated from a MLPNN. the 
     # labels of y_te are the initial labels of the adversarial data. 
-    data = np.load(''.join(['data/full_data_', dataset, '_dt_dt.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_dt_dt.npz']))
     X_tr, y_tr, X_te, y_te, X_adv_dt = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
     
     # lad the deepfool data 
-    data = np.load(''.join(['data/full_data_', dataset, '_mlp_deepfool.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_deepfool.npz']))
     _, _, _, _, X_adv_deepfool = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml']
 
     # load the fgsm data 
-    data = np.load(''.join(['data/full_data_', dataset, '_mlp_fgsm.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_fgsm.npz']))
     _, _, _, _, X_adv_fgsm = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
 
     # load the pgd data 
-    data = np.load(''.join(['data/full_data_', dataset, '_mlp_pgd.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_pgd.npz']))
     _, _, _, _, X_adv_pgd = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
 
     # change the labes; 1=normal; -1=maliicious
