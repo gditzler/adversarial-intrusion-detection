61d5e4ee8cf24024e5e1dcdc2769cfc5bbf629fe
Gregory Ditzler <gregory.ditzler@gmail.com>
2021-04-25 20:54:26 -0700

    needed to set allow_pickle=True

--- experiments.py
+++ experiments.py
@@ -61,19 +61,19 @@ def run_experiment_exploratory(dataset:str='unswnb15',
     # load the data from the npz files. note that all of the X_tr, X_te, y_tr and y_te are the same 
     # regarless of the file. the difference is in how the Xaml data are generated from a MLPNN. the 
     # labels of y_te are the initial labels of the adversarial data. 
-    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_dt_dt.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_dt_dt.npz']), allow_pickle=True)
     X_tr, y_tr, X_te, y_te, X_adv_dt = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
     
     # lad the deepfool data 
-    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_deepfool.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_deepfool.npz']), allow_pickle=True)
     _, _, _, _, X_adv_deepfool = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml']
 
     # load the fgsm data 
-    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_fgsm.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_fgsm.npz']), allow_pickle=True)
     _, _, _, _, X_adv_fgsm = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
 
     # load the pgd data 
-    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_pgd.npz']))
+    data = np.load(''.join(['data/', type, '/full_data_', dataset, '_mlp_pgd.npz']), allow_pickle=True)
     _, _, _, _, X_adv_pgd = data['Xtr'], data['ytr'], data['Xte'], data['yte'], data['Xaml'] 
 
     # change the labes; 1=normal; -1=maliicious
